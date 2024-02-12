662227a49df506422695bf97969a9900b71a981f
FGApps <fostgiayt@gmail.com>
2024-02-12 19:47:06 +0300

    new commit 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        aqwesrdfrdctfvrectfvgqawesxrdcfgvhb ncfvg
+//        aqwesrdfrdctfvrectfvgqawesxrdcfgvhb ncfvg 1234
     }
 }
